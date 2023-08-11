b1fad84d5020f5245fbbf4846ee69736a3b75cc8
kourosh s <kourosh23@hotmail.com>
2023-08-11 23:33:21 +0200

    couponapi gets

---
+++ CouponApi/Controllers/CouponsController.cs
@@ -0,0 +1,44 @@
+﻿using CouponApi.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CouponApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponsController : ControllerBase
+    {
+        private ApplicationDbContext _db { get; set; }
+        private readonly ILogger<CouponsController> _logger;
+
+        public CouponsController(ApplicationDbContext db, ILogger<CouponsController> logger)
+        {
+            _db = db;
+            _logger = logger;
+        }
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                return _db.Coupons.ToList();
+            }
+            catch (Exception e)
+            {
+            }
+            return null;
+        }
+        [HttpGet("{id:int}")]
+        public object Get(int id)
+        {
+            try
+            {
+                return _db.Coupons.FirstOrDefault(x => x.Id == id);
+            }
+            catch (Exception e)
+            {
+            }
+            return null;
+        }
+    }
+}
