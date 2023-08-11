354733a077035ce3f54c1e315cf02595a6f27ca8
kourosh s <kourosh23@hotmail.com>
2023-08-11 22:42:57 +0200

    couponapi models, dto & nugets

---
+++ CouponApi/Models/Coupon.cs
@@ -0,0 +1,11 @@
+﻿namespace CouponApi.Models
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public double Discount { get; set; }
+        public int MinAmount { get; set; }
+        //public DateTime LastUpdated { get; set; }
+    }
+}
