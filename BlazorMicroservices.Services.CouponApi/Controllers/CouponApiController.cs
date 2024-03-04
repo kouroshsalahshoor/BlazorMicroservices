3be94791b5400aa1ff0579521541c179c76e8eca
kourosh <kourosh23@hotmail.com>
2024-03-04 11:39:09 +0100

    api controller & gets

---
+++ BlazorMicroservices.Services.CouponApi/Controllers/CouponApiController.cs
@@ -0,0 +1,44 @@
+﻿using BlazorMicroservices.Services.CouponApi.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorMicroservices.Services.CouponApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CouponApiController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                return _db.Coupons.ToList();
+            }
+            catch (Exception ex)
+            {
+            }
+            return null;
+        }
+
+        [HttpGet("{id:int}")]
+        public object Get(int id)
+        {
+            try
+            {
+                return _db.Coupons.First(x=> x.Id == id);
+            }
+            catch (Exception ex)
+            {
+            }
+            return null;
+        }
+    }
+}
