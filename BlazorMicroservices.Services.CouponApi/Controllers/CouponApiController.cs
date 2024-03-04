ac46696b523a7ae29ac2a2c44786449468fb20a3
kourosh <kourosh23@hotmail.com>
2024-03-04 12:12:35 +0100

    crud endpoints

--- BlazorMicroservices.Services.CouponApi/Controllers/CouponApiController.cs
+++ BlazorMicroservices.Services.CouponApi/Controllers/CouponApiController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using BlazorMicroservices.Services.CouponApi.Data;
+using BlazorMicroservices.Services.CouponApi.Models;
 using BlazorMicroservices.Services.CouponApi.Models.Dtos;
 using BlazorMicroservices.Services.CouponApi.Utilities;
 using Microsoft.AspNetCore.Http;
@@ -45,6 +46,79 @@ namespace BlazorMicroservices.Services.CouponApi.Controllers
             {
                 var result = _db.Coupons.First(x => x.Id == id);
                 _response.Result = _mapper.Map<CouponDto>(result);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccessful = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpGet("getbycode/{code}")]
+        public ResponseDto Get(string code)
+        {
+            try
+            {
+                var result = _db.Coupons.First(x => x.Code.ToLower() == code.ToLower());
+                _response.Result = _mapper.Map<CouponDto>(result);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccessful = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpPost]
+        public ResponseDto Post([FromBody] CouponDto dto)
+        {
+            try
+            {
+                var model = _mapper.Map<Coupon>(dto);
+                var result = _db.Coupons.Add(model);
+                _db.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDto>(result.Entity);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccessful = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpPut]
+        public ResponseDto Put([FromBody] CouponDto dto)
+        {
+            try
+            {
+                var model = _mapper.Map<Coupon>(dto);
+                var result = _db.Coupons.Update(model);
+                _db.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDto>(result.Entity);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccessful = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpDelete("{id:int}")]
+        public ResponseDto Delete(int id)
+        {
+            try
+            {
+                var model = _db.Coupons.First(x => x.Id == id);
+                var result = _db.Coupons.Remove(model);
+                _db.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDto>(result.Entity);
             }
             catch (Exception ex)
             {
