71e38f71fe1e7682543c0297abf47bd2bb2ba8e6
kourosh <kourosh23@hotmail.com>
2024-03-05 12:04:57 +0100

    Blazor project & RespondDto & RequestDto

---
+++ BlazorMicroservices.Web/Utilities/RequestDto.cs
@@ -0,0 +1,12 @@
+﻿using static BlazorMicroservices.Web.Utilities.SD;
+
+namespace BlazorMicroservices.Web.Utilities
+{
+    public class RequestDto
+    {
+        public ApiType ApiType { get; set; } = ApiType.Get;
+        public string Url { get; set; }
+        public object Data { get; set; }
+        public string Token { get; set; }
+    }
+}
