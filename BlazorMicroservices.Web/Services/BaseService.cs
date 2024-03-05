5793a0d653f157cb411738cfc123a1431dc9665c
kourosh <kourosh23@hotmail.com>
2024-03-05 12:43:18 +0100

    BaseService in wed project

---
+++ BlazorMicroservices.Web/Services/BaseService.cs
@@ -0,0 +1,78 @@
+﻿using BlazorMicroservices.Web.Services.IServices;
+using BlazorMicroservices.Web.Utilities;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+using static BlazorMicroservices.Web.Utilities.SD;
+
+namespace BlazorMicroservices.Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
+        {
+            try
+            {
+                var client = _httpClientFactory.CreateClient("xxx");
+                HttpRequestMessage message = new();
+                message.Headers.Add("Accept", "application/json");
+
+                //token
+
+                message.RequestUri = new Uri(requestDto.Url);
+                if (requestDto.Data is not null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(requestDto.Data), Encoding.UTF8, "application/json");
+                }
+
+                switch (requestDto.ApiType)
+                {
+                    case ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                //response
+                var apiResponse = await client.SendAsync(message);
+                switch (apiResponse.StatusCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        return new ResponseDto { IsSuccessful = false, Message = "Not Found" };
+                    case HttpStatusCode.Forbidden:
+                        return new ResponseDto { IsSuccessful = false, Message = "Forbidden" };
+                    case HttpStatusCode.Unauthorized:
+                        return new ResponseDto { IsSuccessful = false, Message = "Unauthorized" };
+                    case HttpStatusCode.InternalServerError:
+                        return new ResponseDto { IsSuccessful = false, Message = "Internal Server Error" };
+                    default:
+                        var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                        var apiResponseDto = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+                        return apiResponseDto;
+                }
+            }
+            catch (Exception ex)
+            {
+                return new ResponseDto
+                {
+                    IsSuccessful = false,
+                    Message = ex.Message.ToString()
+                };
+            }
+        }
+    }
+}
---
+++ BlazorMicroservices.Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,9 @@
+﻿using BlazorMicroservices.Web.Utilities;
+
+namespace BlazorMicroservices.Web.Services.IServices
+{
+    public interface IBaseService
+    {
+        Task<ResponseDto?> SendAsync(RequestDto requestDto);
+    }
+}
